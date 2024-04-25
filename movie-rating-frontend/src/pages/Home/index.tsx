fb01242a7a76ad586fa786d2dcc772168ecc4d7e
erblink <118224412+erblink@users.noreply.github.com>
2024-04-25 17:37:10 +0200

    Better styling for movie cards

--- movie-rating-frontend/src/pages/Home/index.tsx
+++ movie-rating-frontend/src/pages/Home/index.tsx
@@ -1,9 +1,13 @@
-import { Card, Image } from 'semantic-ui-react';
 import { useEffect, useState } from 'react';
+import { Card, Image, Grid } from 'semantic-ui-react';
 import { fetchMovieData } from './mutations';
 import { MovieData } from './mutations';
+import { Link } from 'react-router-dom';
+
+
 export const MovieList = () => {
     const [movies, setMovies] = useState<MovieData[]>([]);
+    const [hoveredCardId, setHoveredCardId] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -18,23 +22,74 @@ export const MovieList = () => {
         fetchData();
     }, []);
 
+    const handleCardHover = (id: string | null) => {
+        setHoveredCardId(id);
+    };
+
     return (
-        <Card.Group>
+       
+        <Grid columns={4} stackable>
             {movies.map((movie) => (
-                <Card key={movie.id}>
-                    <Image src={movie.coverImageUrl} wrapped ui={false} />
-                    <Card.Content>
-                        <Card.Header>{movie.title}</Card.Header>
-                        <Card.Meta>{movie.year}</Card.Meta>
-                        <Card.Description>{movie.description}</Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                        <p>Average Rating: {movie.avgRating}</p>
-                    </Card.Content>
-                </Card>
+
+                <Grid.Column style = {{zIndex:'0'}} key={movie.id}>
+                    <Link to={`/movie/${encodeURIComponent(movie.title)}`} key={movie.id}>
+                    <Card
+                        onMouseEnter={() => handleCardHover(movie.id)}
+                        onMouseLeave={() => handleCardHover(null)}
+                        style={{
+                            marginTop: '20px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-between',
+                            boxShadow: hoveredCardId === movie.id ? '0 4px 8px rgba(0, 0, 0, 0.2)' : 'none',
+                            backgroundColor: hoveredCardId === movie.id ? '#F2F2F2' : 'aliceblue',
+                            border: '1.5px solid black',
+                            borderRadius: '5px',
+                            transition: 'box-shadow 0.3s, background-color 0.3s'
+                        }}
+                    >
+                        <div
+                            style={{
+                                overflow:'hidden',
+                                borderRadius: '10px 10px 0 0',
+                            }}
+                        >
+                            <Image
+                                src={movie.coverImageUrl}
+                                wrapped
+                                ui={false}
+                                style={{
+                                    width: '100%',
+                                    height: 'auto',
+                                    borderRadius:"30px"
+                                }}
+                            />
+                        </div>
+                        <Card.Content style={{ padding: '16px' }}>
+                            <Card.Header style={{ fontSize: '1.2rem', fontWeight: 'bold', color: '#333', marginBottom: '8px' }}>
+                                {movie.title}
+                            </Card.Header>
+                            <Card.Meta style={{ fontSize: '0.9rem', color: '#777', marginTop: '8px' }}>
+                                {movie.year}
+                            </Card.Meta>
+                            <Card.Description style={{ fontSize: '0.8rem',overflow:'hidden' }}>
+                                {movie.description}
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content style={{ padding: '16px' }}>
+                            <p style={{ marginTop: '8px', fontSize: '16px', color: '#333' }}>
+                                Average Rating: {movie.avgRating}
+                            </p>
+                        </Card.Content> 
+                    </Card>
+                    </Link>
+                </Grid.Column>
+               
             ))}
-        </Card.Group>
+        </Grid>
+       
     );
+    
 };
 
 export default MovieList;
